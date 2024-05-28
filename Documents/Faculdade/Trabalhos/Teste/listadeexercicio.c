961dff6ab0e4fdc8ae9e14b4186eab3271dfe20c
VictorGAC <victorgacunha@gmail.com>
2024-05-27 22:04:04 -0300

    teste

---
+++ Documents/Faculdade/Trabalhos/Teste/listadeexercicio.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    // Imprime a mensagem na tela
+    printf("Hello, World!");
+
+    // Retorna 0 para indicar que o programa foi executado com sucesso
+    return 0;
+}
